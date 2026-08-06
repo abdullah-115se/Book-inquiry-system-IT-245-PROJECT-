1daba9c995fa94f32265c4cd1861e74d720f2e0c
abdullah-115se <abdullah-ofaidli@outlook.com>
2026-08-06 22:23:22 +0300

    Update DataLoader.java

--- DataLoader.java
+++ DataLoader.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class DataLoader {
 
-    // Automatically generates N fake books for Phase 2 benchmarking
+    // Automatically generates N fake books for benchmarking
     public static List<Book> generateLargeDataset(int count) {
         List<Book> books = new ArrayList<>();
         for (int i = 1; i <= count; i++) {
